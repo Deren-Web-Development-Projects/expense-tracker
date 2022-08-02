d943d0e9b9ee0b92c1343ee2b4f86b758d532f08
Deren B <dbozerprogramming@gmail.com>
2022-08-02 11:25:59 -0400

    Model Classes setup

---
+++ ExpenseTracker/ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContext options):base(options)
+        {
+
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+
